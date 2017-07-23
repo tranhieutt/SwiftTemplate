053b24d385458c49cdb11593c54788b6ecd2d63f
Tran Trung Hieu <tranhieutt@gmail.com>
2017-07-23 11:44:45 +0700

    Init project

---
+++ SwiftTemplate/SwiftTemplate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftTemplate
+//
+//  Created by Tran Trung Hieu on 7/23/17.
+//  Copyright © 2017 Tran Trung Hieu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
